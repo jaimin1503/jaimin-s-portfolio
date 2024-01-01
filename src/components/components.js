346f90904e923194a9942f68d772f098e74cdf69
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-01-01 08:48:28 +0530

    added styles and image

--- src/components/components.js
+++ src/components/components.js
@@ -3,10 +3,6 @@ window.addEventListener("load", (event) => {
     mouseX = 0;
   var yp = 0,
     mouseY = 0;
-  var xpDot = 0,
-    mouseXDot = 0; // Changed variable name to mouseXDot
-  var ypDot = 0,
-    mouseYDot = 0; // Changed variable name to mouseYDot
 
   const cursorFollower = document.querySelector(".cursorFollower");
 
@@ -14,7 +10,6 @@ window.addEventListener("load", (event) => {
     mouseX = e.pageX;
     mouseY = e.pageY;
   });
-
   setInterval(function () {
     xp += (mouseX - xp) / 15;
     yp += (mouseY - yp) / 15;
@@ -25,4 +20,10 @@ window.addEventListener("load", (event) => {
       cursorFollower.style.top = yp + "px";
     }
   }, 20);
+  document
+    .querySelector(".developer-img")
+    .addEventListener("mouseenter", function () {
+      cursorFollower.style.transform = "translate(-50px, -50px) scale(0)";
+      alert("entered");
+    });
 });
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,22 +2,28 @@ import Navbar from "../../components/Navbar";
 import Welcome from "../../components/homepage/Welcome";
 import "../../components/Styles.css";
 import "../../components/components.js";
+import developer from "../assets/developer.png";
 
 const HomePage = () => {
   return (
     <div className=" bg-[#092635] h-screen w-screen">
       <Navbar />
-      <div className="welcome flex md:flex-row justify-center items-center flex-col">
-        <div className=" md:mb-10">
-          <Welcome />
+      <div className=" flex justify-center">
+        <div className="developer ml-10 pointer-events-none">
+          <img className=" md:h-[60vh] developer-img " src={developer} alt="" />
         </div>
-        <div className="intro md:mt-20">
-          <h1 className=" md:-ml-40 text-balance lg:text-8xl sm:text-7xl text-6xl max-w-xl text-[#B19470] font-poiret font-extrabold text-center md:mt-0 -mt-10">
-            Web Designer &amp; Developer
-          </h1>
+        <div className="welcome flex md:flex-row justify-center items-center flex-col">
+          <div className=" md:mb-10">
+            <Welcome />
+          </div>
+          <div className="intro_home md:mt-20">
+            <h1 className=" md:-ml-40 text-balance lg:text-8xl sm:text-7xl text-6xl max-w-xl text-[#F8FAE5] font-poiret font-extrabold text-center md:mt-0 -mt-10 text-shadow">
+              Web Designer &amp; Developer
+            </h1>
+          </div>
         </div>
       </div>
-      <div className="headline  lg:text-6xl sm:text-5xl text-4xl text-wrap text-[#F8FAE5] font-poiret font-extrabold text-center">
+      <div className="headline  lg:text-5xl sm:text-5xl text-4xl text-wrap text-[#F8FAE5] font-poiret font-extrabold text-center">
         <h1 className=" mt-10">
           "Innovative <span className=" text-[#43766C]">MERN</span> Solutions
           for Your Digital Success Story"
