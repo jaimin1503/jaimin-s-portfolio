8d58926dedb23411342df43f724731914bbff123
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-01-01 10:02:55 +0530

    home page complited

--- src/components/homepage/Welcome.jsx
+++ src/components/homepage/Welcome.jsx
@@ -3,7 +3,7 @@ import "../Styles.css";
 const Welcome = () => {
   return (
     <div>
-      <div className=" m-20 lg:h-[20vw] lg:w-[20vw] w-[30vw] h-[30vw] rounded-full  bg-[#9EC8B9] flex justify-center items-center welcome-circle flex-col bg-blend-multiply">
+      <div className=" my-5 md:m-10 lg:h-[25vw] lg:w-[25vw] xl:w-[20vw] xl:h-[20vw] w-[30vw] h-[30vw] rounded-full  bg-[#9EC8B9] flex justify-center items-center welcome-circle flex-col bg-blend-multiply">
         <h1 className="lg:text-4xl text-3xl font-poiret font-bold">Welcome!</h1>
         <h2 className=" text-2xl lg:text-3xl font-poiret font-semibold">
           to Jaimin's
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,33 +1,38 @@
 import Navbar from "../../components/Navbar";
 import Welcome from "../../components/homepage/Welcome";
 import "../../components/Styles.css";
-import "../../components/components.js";
+import "./cursor.js";
 import developer from "../assets/developer.png";
 
 const HomePage = () => {
   return (
-    <div className=" bg-[#092635] h-screen w-screen">
+    <div className=" bg-[#092635]">
       <Navbar />
-      <div className=" flex justify-center">
+      <div className=" flex justify-center flex-col items-center lg:flex-row">
         <div className="developer ml-10 pointer-events-none">
-          <img className=" md:h-[60vh] developer-img " src={developer} alt="" />
+          <img
+            className=" object-cover h-[40vh] md:h-[60vh] developer-img "
+            src={developer}
+            alt="dev"
+          />
         </div>
         <div className="welcome flex md:flex-row justify-center items-center flex-col">
           <div className=" md:mb-10">
             <Welcome />
           </div>
           <div className="intro_home md:mt-20">
-            <h1 className=" md:-ml-40 text-balance lg:text-8xl sm:text-7xl text-6xl max-w-xl text-[#F8FAE5] font-poiret font-extrabold text-center md:mt-0 -mt-10 text-shadow">
+            <h1 className=" md:-ml-40 text-balance lg:text-7xl sm:text-6xl text-6xl max-w-xl text-[#F8FAE5] font-poiret font-extrabold text-center md:mt-0 -mt-10 text-shadow">
               Web Designer &amp; Developer
             </h1>
           </div>
         </div>
       </div>
-      <div className="headline  lg:text-5xl sm:text-5xl text-4xl text-wrap text-[#F8FAE5] font-poiret font-extrabold text-center">
-        <h1 className=" mt-10">
+      <div className="headline  lg:text-5xl sm:text-5xl text-4xl text-wrap text-[#F8FAE5] font-poiret font-extrabold text-center py-10">
+        <h1 className="py-5 lg:py-10">
           "Innovative <span className=" text-[#43766C]">MERN</span> Solutions
           for Your Digital Success Story"
         </h1>
+        <h1 className="py-5 lg:py-10">"Innovate, Develop, Deliver"</h1>
       </div>
       <span
         id="cursorFollower"
