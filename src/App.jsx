e8f1b49dad0292bd3d7250b6efb8b4856f649780
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-01-02 12:09:04 +0530

    added animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import AboutMe from "./pages/aboutMe/AboutMe";
 import HomePage from "./pages/home/HomePage";
+import "./pages/cursor.js";
+// import "./pages/cursor.css";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <>
+      {/* <span
+        id="cursorFollower"
+        className="cursorFollower bg-gray-100 md:h-[10rem] md:w-[10rem] h-[7rem] w-[7rem] "
+      ></span> */}
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={8}
+        color="193, 11, 111"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
       <HomePage />
       <AboutMe />
     </>
--- src/pages/aboutMe/AboutMe.jsx
+++ src/pages/aboutMe/AboutMe.jsx
@@ -8,56 +8,62 @@ import email from "./assets/email.svg";
 const AboutMe = () => {
   return (
     <div className=" bg-[#9EC8B9] h-screen w-screen">
-      <div className="about-me py-10 flex justify-center items-center">
-        <div className="info-text p-10">
-          <h2 className=" text-4xl font-poiret py-5 text-[#092635] font-extrabold">
-            Hello, I am Jaimin Viramgama
-          </h2>
-          <h2 className=" text-4xl font-poiret py-5 text-[#092635] font-extrabold">
-            I do Front-end development.
-          </h2>
-          <p className=" text-2xl font-poiret max-w-xl text-[#092635]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-            ipsum consequatur saepe omnis illum nostrum. In architecto, eos
-            adipisci ducimus id natus delectus nisi fuga qui consectetur aperiam
-            <br />
-            dolorum beatae. Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Aspernatur, at alias temporibus eos architecto cupiditate
-            ullam perferendis vitae id dolor minima? Molestias, consequatur
-            saepe molestiae aut iure hic provident ipsa. Lorem ipsum dolor, sit
-          </p>
-        </div>
-        <div className="my-image">
-          <img className="w-[30vw] object-center" src={jaimin} alt="" />
-        </div>
-      </div>
-      <div className=" links flex ml-20 p-10 font-poiret font-extrabold">
-        <button className=" py-2 px-5">Schedule a meet</button>
-        <button className=" py-2 px-5">Resume {"->"}</button>
-      </div>
+      <div className=" flex flex-col">
+        <div className="about-me py-10 flex justify-center ">
+          <div className="info-text p-10 flex flex-col items-start">
+            <h2 className=" text-4xl font-poiret py-5 text-[#092635] font-extrabold">
+              Hello, I am Jaimin Viramgama
+            </h2>
+            <h2 className=" text-4xl font-poiret py-5 text-[#092635] font-extrabold">
+              I do Front-end development.
+            </h2>
+            <p className=" text-2xl font-poiret max-w-xl text-[#092635]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+              ipsum consequatur saepe omnis illum nostrum. In architecto, eos
+              adipisci ducimus id natus delectus nisi fuga qui consectetur
+              aperiam
+              <br />
+              dolorum beatae. Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Aspernatur, at alias temporibus eos architecto
+              cupiditate ullam perferendis vitae id dolor minima? Molestias,
+              consequatur saepe molestiae aut iure hic provident ipsa. Lorem
+              ipsum dolor, sit
+            </p>
+            <div className=" flex flex-col mt-10">
+              <div className=" links flex font-poiret my-5 font-extrabold">
+                <button className=" py-2 px-5">Schedule a meet</button>
+                <button className=" py-2 px-5">Resume {"->"}</button>
+              </div>
 
-      <div className="social-links ml-20 m-10 w-fit p-3 rounded-2xl">
-        <div className="flex relative opacity-100">
-          <Link to="https://www.linkedin.com/in/jaimin-viramgama-487485233/">
-            <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl">
-              <img src={linkedin} alt="linkedin" />
+              <div className="social-links w-fit p-3 rounded-2xl">
+                <div className="flex relative opacity-100">
+                  <Link to="https://www.linkedin.com/in/jaimin-viramgama-487485233/">
+                    <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl social-logos">
+                      <img src={linkedin} alt="linkedin" />
+                    </div>
+                  </Link>
+                  <Link to="https://github.com/jaimin1503">
+                    <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl social-logos">
+                      <img src={github} alt="github" />
+                    </div>
+                  </Link>
+                  <Link to="https://www.instagram.com/jaimin_15.3/">
+                    <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl social-logos">
+                      <Instagram />
+                    </div>
+                  </Link>
+                  <Link to="mailto:jaiminviramgama152@gmail.com">
+                    <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl social-logos">
+                      <img src={email} alt="email" />
+                    </div>
+                  </Link>
+                </div>
+              </div>
             </div>
-          </Link>
-          <Link to="https://github.com/jaimin1503">
-            <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl">
-              <img src={github} alt="github" />
-            </div>
-          </Link>
-          <Link to="https://www.instagram.com/jaimin_15.3/">
-            <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl">
-              <Instagram />
-            </div>
-          </Link>
-          <Link to="mailto:jaiminviramgama152@gmail.com">
-            <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl">
-              <img src={email} alt="email" />
-            </div>
-          </Link>
+          </div>
+          <div className="my-image">
+            <img className="w-[30vw] object-center" src={jaimin} alt="jaimin" />
+          </div>
         </div>
       </div>
     </div>
