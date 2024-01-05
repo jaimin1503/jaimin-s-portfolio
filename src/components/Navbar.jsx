5240adecb5e8ed8c4d382c369562c9783f382ef6
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-01-05 15:44:52 +0530

    responsive nave bar
    minor fix remaining

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import "./Styles.css";
 import useCustomCursor from "./useCustomCursor";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Navbar = () => {
-  const { cursorVariant, variants, textEnter, textLeave } = useCustomCursor();
+  const { cursorVariant, variants } = useCustomCursor();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
@@ -14,24 +16,83 @@ const Navbar = () => {
       />
       <nav>
         <div className="nav_items">
-          <ul
-            onMouseEnter={textEnter}
-            onMouseLeave={textLeave}
-            className=" flex justify-center items-center font-poiret"
+          <div className=" flex md:justify-center right-0 pr-10 absolute md:relative ">
+            <motion.ul
+              className={`flex flex-col md:flex-row font-poiret relative ${
+                !isOpen ? "hidden" : "visible"
+              } visible`}
+            >
+              <motion.li className="items text-lg my-4 mx-8 cursor-pointer cool-link">
+                HOME
+              </motion.li>
+              <motion.li className="items text-lg my-4 mx-8 cursor-pointer cool-link">
+                ABOUT ME
+              </motion.li>
+              <motion.li className="items text-lg my-4 mx-8 cursor-pointer cool-link">
+                SKILLS
+              </motion.li>
+              <motion.li className="items text-lg my-4 mx-8 cursor-pointer cool-link">
+                CONTACT
+              </motion.li>
+            </motion.ul>
+          </div>
+        </div>
+        <div className="menu-button right-3 top-0 py-2 absolute md:hidden ">
+          <svg
+            onClick={() => setIsOpen(!isOpen)}
+            style={{ cursor: "pointer" }}
+            className="hb"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 10 10"
+            height="40px"
+            width="40px"
+            stroke="#eee"
+            strokeWidth=".7"
+            fill="rgba(0,0,0,0)"
+            strokeLinecap="round"
           >
-            <li className={`items text-lg my-4 mx-8 cursor-pointer cool-link`}>
-              HOME
-            </li>
-            <li className={`items text-lg my-4 mx-8 cursor-pointer cool-link`}>
-              ABOUT ME
-            </li>
-            <li className={`items text-lg my-4 mx-8 cursor-pointer cool-link`}>
-              SKILLS
-            </li>
-            <li className={`items text-lg my-4 mx-8 cursor-pointer cool-link`}>
-              CONTACT
-            </li>
-          </ul>
+            <path d="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7">
+              <animate
+                dur="0.2s"
+                attributeName="d"
+                values="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7;M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7"
+                fill="freeze"
+                begin="start.begin"
+              />
+              <animate
+                dur="0.2s"
+                attributeName="d"
+                values="M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7;M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7"
+                fill="freeze"
+                begin="reverse.begin"
+              />
+            </path>
+            <rect width="10" height="10" stroke="none">
+              <animate
+                dur="2s"
+                id="reverse"
+                attributeName="width"
+                begin="click"
+              />
+            </rect>
+            <rect width="10" height="10" stroke="none">
+              <animate
+                dur="0.001s"
+                id="start"
+                attributeName="width"
+                values="10;0"
+                fill="freeze"
+                begin="click"
+              />
+              <animate
+                dur="0.001s"
+                attributeName="width"
+                values="0;10"
+                fill="freeze"
+                begin="reverse.begin"
+              />
+            </rect>
+          </svg>
         </div>
       </nav>
     </div>
